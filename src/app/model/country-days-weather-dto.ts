aea9558c2fa5702c8e1f30a6fe509aa492e03c10
Sashikanth lomada <vsai85550@gmail.com>
2023-09-15 00:28:19 +0530

    worked on the weather app

---
+++ src/app/model/country-days-weather-dto.ts
@@ -0,0 +1,54 @@
+export class CountryDaysWeatherDto {
+    lat: number
+    lon: number
+    timezone: string
+    timezone_offset: number
+    daily: Daily[]
+  }
+  
+  export class Daily {
+    dt: number
+    sunrise: number
+    sunset: number
+    moonrise: number
+    moonset: number
+    moon_phase: number
+    temp: Temp
+    feels_like: FeelsLike
+    pressure: number
+    humidity: number
+    dew_point: number
+    wind_speed: number
+    wind_deg: number
+    wind_gust: number
+    weather: Weather[]
+    clouds: number
+    pop: number
+    uvi: number
+    rain?: number;
+    convertedDate:string;
+  }
+  
+  export class Temp {
+    day: number
+    min: number
+    max: number
+    night: number
+    eve: number
+    morn: number
+  }
+  
+  export class FeelsLike {
+    day: number
+    night: number
+    eve: number
+    morn: number
+  }
+  
+  export class Weather {
+    id: number
+    main: string
+    description: string
+    icon: string
+  }
+  
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CommonService } from '../service/common.service';
+import { CountryDto } from '../model/country-dto';
+import {  CountryWeatherDto } from '../model/country-weather';
+import { CountryDaysWeatherDto } from '../model/country-days-weather-dto';
 
 @Component({
   selector: 'app-weather',
@@ -7,4 +11,70 @@ import { Component } from '@angular/core';
 })
 export class WeatherComponent {
 
+  constructor(public _commonService:CommonService){}
+  searchCountry:string;
+  searchHistory:CountryWeatherDto[]=[];
+  wetherOfCountryData:CountryWeatherDto;
+  fiveDaysResponse:CountryDaysWeatherDto;
+  searchCountryByName(){
+    this._commonService.searchCountryLocation(this.searchCountry)
+    .subscribe({
+      next: (res) => { 
+        console.log(res);
+        let wetherOfCountryData:CountryWeatherDto;
+        wetherOfCountryData = res;
+        wetherOfCountryData.calculatedCelcius = this._commonService.toCelsius(res.main.temp);
+        this.wetherOfCountryData = wetherOfCountryData;
+        this.searchHistory.push(this.wetherOfCountryData);
+       },     // nextHandler
+      error: (error) => { 
+        console.log(error);
+        
+       },    // errorHandler 
+      //complete: () => { ... }, // completeHandler
+  });
+    
+  }
+  fetchWeatherHistoryOfFiveDays(country:CountryWeatherDto){
+    this._commonService.findUpComingFiveDaysWeather(country)
+    .subscribe({
+      next: (res) => { 
+        console.log(res);
+        let response=res;
+        response.daily.forEach((obj)=>{
+          obj.convertedDate = new Date(obj.dt * 1000).toLocaleDateString("en", {
+            weekday: "long",
+          });
+
+        })
+        this.fiveDaysResponse = response;
+
+       },     // nextHandler
+      error: (error) => { 
+        console.log(error);
+        
+       },    // errorHandler 
+      //complete: () => { ... }, // completeHandler
+  });
+  }
+  fetchWeatherHistory(country:CountryWeatherDto){
+    this._commonService.findWeather(country)
+    .subscribe({    
+      next: (res) => { 
+        console.log(res);
+        // this.searchHistory.push(res[0]);
+        let wetherOfCountryData:CountryWeatherDto;
+        wetherOfCountryData = res;
+        wetherOfCountryData.calculatedCelcius = this._commonService.toCelsius(res.main.temp);
+        this.wetherOfCountryData = wetherOfCountryData;
+       },     // nextHandler
+      error: (error) => { 
+        console.log(error);
+        
+       },    // errorHandler 
+      //complete: () => { ... }, // completeHandler
+  });
+  }
 }
+
+
